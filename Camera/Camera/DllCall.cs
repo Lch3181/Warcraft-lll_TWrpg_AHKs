77ccafc6b862149088125e1e253f60520c8d669a
Lch#3181 <34583022+Lch3181@users.noreply.github.com>
2020-11-24 12:54:26 -0500

    added C# project for wc3 camera adjusting

---
+++ Camera/Camera/DllCall.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Camera
+{
+    internal static class DllCall
+    {
+        [DllImport("kernel32", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool ReadProcessMemory
+        (
+            [In] IntPtr hProcess,
+            [In] IntPtr lpBaseAddress,
+            [Out] byte[] lpBuffer,
+            [In] int dwSize,
+            [Out] out int lpNumberOfBytesRead
+        );
+
+        [DllImport("kernel32", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool VirtualProtectEx
+        (
+            [In] IntPtr hProcess,
+            [In] IntPtr lpAddress,
+            [In] int dwSize,
+            [In] uint flNewProtect,
+            [Out] out uint lpflOldProtect
+        );
+
+        [DllImport("kernel32", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool WriteProcessMemory
+        (
+            [In] IntPtr hProcess,
+            [In] IntPtr lpBaseAddress,
+            [In] byte[] lpBuffer,
+            [In] int nSize,
+            [Out] out int lpNumberOfBytesWritten
+        );
+
+        [DllImport("user32", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool PostMessage
+        (
+            [In] IntPtr hWnd,
+            [In] uint Msg,
+            [In] uint wParam,
+            [In] uint lParam
+        );
+    }
+}
---
+++ Camera/Camera/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using static Camera.DllCall;
+
+namespace Camera
+{
+    class Program
+    {
+        static IntPtr Handle;
+        static IntPtr MainWindowHandle;
+        static IntPtr GameDllOffSet;
+        static int Main(string[] args)
+        {
+#if DEBUG
+            args = new[] { "1650.0", "90.0", "304.0" };
+#endif
+            //camera.exe distance X Y
+            if (args.Length < 3)
+                return -1; // not enough args
+
+            //get wc3 process handle and game.dll offset
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist)
+            {
+                if (theprocess.ProcessName == "Warcraft III")
+                {
+                    Handle = theprocess.Handle;
+                    MainWindowHandle = theprocess.MainWindowHandle;
+                    foreach (ProcessModule module in theprocess.Modules)
+                    {
+                        if (module.ModuleName.ToLower() == "game.dll")
+                        {
+                            GameDllOffSet = module.BaseAddress;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Handle: " + MainWindowHandle);
+            Console.WriteLine("GameDll: " + GameDllOffSet);
+            //apply camera settings
+            CameraDistance(float.Parse(args[0]));
+            CameraAngleX(float.Parse(args[1]));
+            CameraAngleY(float.Parse(args[2]));
+            CameraInit();
+            return 0;
+        }
+        public static void CameraInit()
+        {
+            PostMessage(MainWindowHandle, 0x100, 27, 0);
+            PostMessage(MainWindowHandle, 0x101, 27, 0);
+            PostMessage(MainWindowHandle, 0x100, 46, 0);
+            PostMessage(MainWindowHandle, 0x101, 46, 0);
+        }
+        public static void CameraDistance(float value)
+        {
+            if (GameDllOffSet == IntPtr.Zero) return;
+            Patch(GameDllOffSet + 0xA8F300, BitConverter.GetBytes(value));
+        }
+        public static void CameraAngleX(float value)
+        {
+            if (GameDllOffSet == IntPtr.Zero) return;
+            Patch(GameDllOffSet + 0xA8F2D0, BitConverter.GetBytes(value));
+        }
+        public static void CameraAngleY(float value)
+        {
+
+            if (GameDllOffSet == IntPtr.Zero) return;
+            Patch(GameDllOffSet + 0xA8F2F0, BitConverter.GetBytes(value));
+        }
+        internal static void Patch(IntPtr offset, params byte[] buffer) => Patch(offset, buffer.Length, buffer);
+        internal static void Patch(IntPtr offset, int size, params byte[] buffer)
+        {
+            VirtualProtectEx(Handle, offset, size, 0x40, out uint lpflOldProtect);
+            WriteProcessMemory(Handle, offset, buffer, size, out _);
+            VirtualProtectEx(Handle, offset, size, lpflOldProtect, out _);
+        }
+        internal static byte[] Bring(IntPtr Offset, int size)
+        {
+            byte[] lpBuffer = new byte[size];
+            VirtualProtectEx(Handle, Offset, size, 0x40, out uint lpflOldProtect);
+            bool ret = ReadProcessMemory(Handle, Offset, lpBuffer, size, out _);
+            VirtualProtectEx(Handle, Offset, size, lpflOldProtect, out _);
+            return ret ? lpBuffer : null;
+        }
+    }
+}
